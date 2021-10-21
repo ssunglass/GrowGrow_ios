ff60f70bb08c02bdd0185d00ccedab0fbc2280c8
ssunglass <leegm218@naver.com>
2021-10-21 15:22:42 +0900

    Initial Commit

---
+++ GrowGrow_ios/GrowGrow_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowGrow_iosApp.swift
+//  GrowGrow_ios
+//
+//  Created by 김세훈 on 2021/10/21.
+//
+
+import SwiftUI
+
+@main
+struct GrowGrow_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
