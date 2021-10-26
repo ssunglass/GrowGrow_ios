9016c06b8ffc54310f56d92276be84f482722f5c
ssunglass <leegm218@naver.com>
2021-10-26 19:11:26 +0900

    EditProfile #1

--- GrowGrow_ios/Views/EditProfileView.swift
+++ GrowGrow_ios/Views/EditProfileView.swift
@@ -13,12 +13,20 @@ struct EditProfileView: View {
     @State private var fullname: String = ""
     @State private var username: String = ""
     @State private var summary: String = ""
+    @StateObject var viewModel = ViewModel()
     
-    @State private var pickerindex = 0
+    
+    @State private var selectedRegion = 0
+    @State private var selectedDepart = 0
+    @State private var selectedMajor = 0
     
     //퍼센트 단위를 배열로 만들어주고
     let regions = ["서울/경기","강원","충청","대구/경북","전북/전남","제주"]
     let departs = ["인문","사회","공학","자연","교육","의약","예체능"]
+    @State private var urlString: String
+  
+    
+    
    
    
 
@@ -26,12 +34,53 @@ struct EditProfileView: View {
         self._fullname = State(initialValue: initfullname)
         self._username = State(initialValue: initusername)
         self._summary = State(initialValue: initsummary)
+        self._urlString = State(initialValue: "https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=8fa1b6fffaf969b85712d6ea45a921fd&svcType=api&svcCode=MAJOR&contentType=json&gubun=univ_list&univSe=univ&subject=100391&perPage=50")
         
     }
+    
+     func urlControl(){
+         
+         if departs[selectedDepart] == "의약"{
+             
+             urlString = "https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=8fa1b6fffaf969b85712d6ea45a921fd&svcType=api&svcCode=MAJOR&contentType=json&gubun=univ_list&univSe=univ&subject=100396&perPage=50"
+             
+             
+         } else if departs[selectedDepart] == "사회" {
+             
+              urlString = "https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=8fa1b6fffaf969b85712d6ea45a921fd&svcType=api&svcCode=MAJOR&contentType=json&gubun=univ_list&univSe=univ&subject=100392&perPage=50"
+         }
+         else if departs[selectedDepart] == "교육" {
+             
+              urlString = "https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=8fa1b6fffaf969b85712d6ea45a921fd&svcType=api&svcCode=MAJOR&contentType=json&gubun=univ_list&univSe=univ&subject=100394&perPage=50"
+         }
+         else if departs[selectedDepart] == "공학" {
+             
+              urlString = "https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=8fa1b6fffaf969b85712d6ea45a921fd&svcType=api&svcCode=MAJOR&contentType=json&gubun=univ_list&univSe=univ&subject=100392&perPage=50"
+         }
+         else if departs[selectedDepart] == "자연" {
+             
+              urlString = "https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=8fa1b6fffaf969b85712d6ea45a921fd&svcType=api&svcCode=MAJOR&contentType=json&gubun=univ_list&univSe=univ&subject=100395&perPage=50"
+         }
+         else if departs[selectedDepart] == "예체능" {
+             
+              urlString = "https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=8fa1b6fffaf969b85712d6ea45a921fd&svcType=api&svcCode=MAJOR&contentType=json&gubun=univ_list&univSe=univ&subject=100397&perPage=50"
+         } else {
+             urlString = "https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=8fa1b6fffaf969b85712d6ea45a921fd&svcType=api&svcCode=MAJOR&contentType=json&gubun=univ_list&univSe=univ&subject=100391&perPage=50"
+             
+         }
+         
+       
+        
+       
+        
+    }
+    
     
 
     
     var body: some View {
+        
+      
         
         NavigationView {
             
@@ -61,7 +110,7 @@ struct EditProfileView: View {
                
                                //퍼센트 섹션
                                 Section {
-                                    Picker("현재 나의 지역", selection: $pickerindex) {
+                                    Picker("현재 나의 지역", selection: $selectedRegion) {
                                         //0..< = 딕셔너리의 [10]부터 tipPercentage보다 작은값
                                         ForEach(0 ..< regions.count) {
                                             Text("\(self.regions[$0])")
@@ -70,7 +119,7 @@ struct EditProfileView: View {
                                 }
                 
                 Section {
-                    Picker("현재 나의 계열", selection: $pickerindex) {
+                    Picker("현재 나의 계열", selection: $selectedDepart) {
                         //0..< = 딕셔너리의 [10]부터 tipPercentage보다 작은값
                         ForEach(0 ..< departs.count) {
                             Text("\(self.departs[$0])")
@@ -79,13 +128,21 @@ struct EditProfileView: View {
                 }
                 
                 Section {
-                    Picker("현재 나의 전공", selection: $pickerindex) {
-                        //0..< = 딕셔너리의 [10]부터 tipPercentage보다 작은값
-                        ForEach(0 ..< departs.count) {
-                            Text("\(self.departs[$0])")
+                    Picker("현재 나의 전공", selection: $selectedMajor) {
+                     ForEach(0 ..< viewModel.contents.count, id: \.self) {
+                             Text(self.viewModel.contents[$0].mClass)
+                            
                         }
+                    }.onAppear{
+                        urlControl()
+                        viewModel.getJson(urlString: urlString)
+                            
                     }
+                    
                 }
+               
+                
+                
                 
                 
             
@@ -107,6 +164,7 @@ struct EditProfileView: View {
                 }, trailing:
                     HStack {
                         Button(action: {
+                        
                         }) {
                             Image(systemName: "checkmark")
                         }
@@ -122,4 +180,12 @@ struct EditProfileView_Previews: PreviewProvider {
     static var previews: some View {
         EditProfileView(initfullname: "", initusername: "", initsummary: "")
     }
+}
+
+struct DetailView: View {
+    
+    var body: some View{
+        Text("Detail")
+    }
+    
 }
--- GrowGrow_ios/Views/MainView/SearchView.swift
+++ GrowGrow_ios/Views/MainView/SearchView.swift
@@ -7,88 +7,16 @@
 
 import SwiftUI
 
-struct DataSearch: Codable {
-    let dataSearch: Contents
-    
-    
-}
-
-struct Contents: Codable {
-    let content: [MajorInfo]
-}
-
-struct MajorInfo: Hashable,Codable {
-    let lClass: String
-    let facilName: String
-    let majorSeq: String
-    let mClass: String
-    let totalCount: String
-    
-    
-}
-
-class ViewModel: ObservableObject {
-    @Published var contents: [MajorInfo] = []
-    
-     func getJson(){
-        guard let url = URL(string: "https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=8fa1b6fffaf969b85712d6ea45a921fd&svcType=api&svcCode=MAJOR&contentType=json&gubun=univ_list&univSe=univ&subject=100391") else {return}
-        
-        let task = URLSession.shared.dataTask(with: url) { [weak self]data, _, error in
-            guard let data = data, error == nil else {
-                return
-            }
-            //Convert to JSON
-            
-            do {
-                let datas = try
-                JSONDecoder().decode(DataSearch.self, from: data)
-                
-                DispatchQueue.main.async {
-                    self?.contents = datas.dataSearch.content
-                    
-                }
-                
-                
-                
-                
-            } catch{
-                print(error)
-            }
-            
-            
-        }
-         
-         task.resume()
-        
-      
-
-        
-    }
-    
-    
-}
 
 struct SearchView: View {
-    @StateObject var viewModel = ViewModel()
+    
     
     
     var body: some View {
         
-        List{
-            ForEach(viewModel.contents, id: \.self){ content in
-                HStack {
-                    
-                    Text(content.mClass)
-                    
-                }
-            }
-            
-        }
-        .onAppear{
-            viewModel.getJson()
-        }
+      
         
-        
+        Text("Search")
     
        
     
